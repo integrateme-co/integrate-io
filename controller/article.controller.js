55bd373af72a929e967e2ee5cb5744dcb9dd6912
uzair-ali10 <the.uzairali10@gmail.com>
2021-09-21 23:01:27 +0530

    Dynamic Article and API in env

--- controller/article.controller.js
+++ controller/article.controller.js
@@ -1,25 +1,13 @@
-const DEV_API = "MiXvYZyVvqCP1oPjg2AdHhQp";
+const DEV_API = process.env.DEV_API;
 const POST_URL = "https://dev.to/api/articles/";
 const axios = require('axios');
-
-const article = {
-    "article": {
-      "title": "Trying Out vdfferergDev.to API",
-      "published": true,
-      "body_markdown": "Hello I'm trywefghrfgdfing to integrate dev.to API in my new project, so if u see this post published this means that i've integrated it and kindly igore this post :)",
-      "tags": [
-        "discuss",
-        "help"
-      ],
-      "series": "Hello series"
-    }
-  };
 
   const headers = { 
     'api-key': DEV_API
 };
 
 exports.publishArticle = (req, res, next) => {
+  let article = req.body.article;
     try {
         axios.post(POST_URL, article, { headers });
         return res.status(201).send({status: "Created"});
