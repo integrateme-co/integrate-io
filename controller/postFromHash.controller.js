14a83bc0acbd2b54fde902b5c1b4fc41ac19a58f
uzair-ali10 <the.uzairali10@gmail.com>
2021-10-05 11:26:35 +0530

    Refactored Error Response

--- controller/postFromHash.controller.js
+++ controller/postFromHash.controller.js
@@ -51,7 +51,7 @@ exports.postFromHash = async (req, res, next) => {
          if(mediumArticle || devArticle){
            return res.status(201).json({"Message": "Blog Sucessfully Posted"});
          }
-
+         return res.status(400).json({"Error": "None Encountred"});
     }catch(error){
         return res.send(error);
     }
