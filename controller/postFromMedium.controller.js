b9bc0350743ed2bd95b85c7198258cdb7cf9d74f
sanjaybaskaran01 <72266283+sanjaybaskaran01@users.noreply.github.com>
2021-10-11 22:54:21 +0530

    fix: Error handling postFromMedium

--- controller/postFromMedium.controller.js
+++ controller/postFromMedium.controller.js
@@ -4,45 +4,60 @@ const postToHashnode = require("../services/postToHashnode");
 
 function mediumURLparser(URL) {
     const arr = URL.split("/");
-    const result = `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/${arr[3]}`
-    return result;
+    const user = arr[3]
+    const link = `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/${user}`
+    return { user, link };
 }
 
-exports.postFromMedium = async(req, res, next) => {
-    try{
-        const {url, dev, hash, dev_api, hash_api} = req.body;
-        const link = mediumURLparser(url);
-        const {data} = await axios.get(link);        
-        const feed = data;
+exports.postFromMedium = async (req, res, next) => {
+    try {
+        const { url, dev, hash, dev_api, hash_api } = req.body;
+        const { user, link } = mediumURLparser(url);
+        var article;
+        try {
+            var { data } = await axios.get(link);
+            var { items } = data
+            if (items.length != 0) {
+                article = items.filter(item => item.link.split("?")[0] === url)
+                if (article.length == 0) {
+                    res.status(400).json({ "Error": `Incorrect blog URL of ${user}` });
+                }
+                article = {
+                    "title": article[0].title,
+                    "content": article[0].description
+                }
+            }
+            else {
+                res.status(400).json({ "Error": "No Blogs by person" });
+            }
+        }
+        catch (err) {
+            const { status, statusText } = err.response
+            res.status(status).json({ "Error": `${statusText},Please Check your URL` });
+            article = false;
+        }
 
-        const itemArr = feed.items;
-        const article = itemArr.filter(item => item.link.split("?")[0] === url);
         let Devblog;
         let hashBlog;
 
-        //TODO: Below empty array check not working
-        if(!article){
-            res.status(400).json({"Error": "Blog Not found"});
-        }
-        //TODO: Array is comming in article
-        if(dev) {
-            Devblog = await postToDev(article[0], dev_api, "medium");
-            if(!Devblog){
-                return res.status(400).json({"Error": "Unable to publish to Dev.to From Medium"});
+        if (dev) {
+            Devblog = await postToDev(article, dev_api, "medium");
+            if (!Devblog) {
+                return res.status(400).json({ "Error": "Unable to publish to Dev.to From Medium" });
             }
         }
 
-        if(hash){
-            hashBlog = await postToHashnode(article[0], hash_api, "medium");
-            if(!hashBlog){
-                return res.status(400).json({"Error": "Unable to publish to Hashnode from Medium"});
+        if (hash) {
+            hashBlog = await postToHashnode(article, hash_api, "medium");
+            if (!hashBlog) {
+                return res.status(400).json({ "Error": "Unable to publish to Hashnode from Medium" });
             }
         }
-        if(hashBlog || Devblog){
-            return res.status(201).json({"Message": "Blog Sucessfully Posted"});
+        if (hashBlog || Devblog) {
+            return res.status(201).json({ "Message": "Blog Sucessfully Posted" });
         }
-        return res.status(400).json({"Error": "None Encountred"});
-    } catch(error){
+        return res.status(400).json({ "Error": "None Encountred" });
+    } catch (error) {
         console.log(error);
         return error;
     }
